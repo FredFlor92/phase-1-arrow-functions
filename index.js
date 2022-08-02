2136cbb62d831b2c4f715d3d9dafab77f79fa458
Freddy Florial <freddyflorial@gmail.com>
2022-08-02 19:16:44 -0400

    commit tree 10ad8d8e1afb9d31ae8ab3e760b481b60affd15b

---
+++ index.js
@@ -0,0 +1,9 @@
+//has a function called divide - Divide divides 2000/100
+const divide = function(){
+    return (2000/100);
+}
+//arrow function called square - square arrow function takes one parameter and multiplies it times itself
+const square = (num1) => num1 * num1 
+
+//arrow function called add - adds the parameters together 
+const add = (num2, num3) => num2 + num3
